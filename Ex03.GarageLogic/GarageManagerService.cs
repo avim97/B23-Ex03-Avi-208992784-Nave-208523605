47f3b2b2addd875d0a60090c38cc92ae6927b799
Nave Maor <rsd.nave@gmail.com>
2023-05-20 15:51:35 +0300

    added functions

--- Ex03.GarageLogic/GarageManagerService.cs
+++ Ex03.GarageLogic/GarageManagerService.cs
@@ -55,5 +55,61 @@ namespace Ex03.GarageLogic
             CustomerTicket ticketToChangeStatus = m_CustomersTickets[Convert.ToInt32(i_licensePlate)];
             ticketToChangeStatus.VehicleStatus = i_vehicleStatus;
         }
+
+
+        //todo: complete function
+        public void InflateWheelsToMax(string i_licensePlate)
+        {
+            // Vehicle vehicleToInflate = m_Vehicles[Convert.ToInt32(i_licensePlate)];
+            // foreach(Wheel wheel in vehicleToInflate.Wheels)
+            // {
+            //     wheel.InflateWheel(wheel.MaxAirPressure - wheel.CurrentAirPressure);
+            // }
+        }
+
+
+        //todo: complete function
+
+        public void RefuelVehicle(string i_licensePlate, eFuelTypes i_fuelType, float i_amountOfFuelToAdd)
+        {
+            // Vehicle vehicleToRefuel = m_Vehicles[Convert.ToInt32(i_licensePlate)];
+            // if(vehicleToRefuel.EnergySource is FuelSource)
+            // {
+            //     FuelSource fuelSource = vehicleToRefuel.EnergySource as FuelSource;
+            //     fuelSource.Refuel(i_fuelType, i_amountOfFuelToAdd);
+            // }
+            // else
+            // {
+            //     throw new ArgumentException("Vehicle is not fuel based");
+            // }
+        }
+
+
+        //todo: complete function
+
+        public void ChargeVehicle(string i_licensePlate, float i_amountOfMinutesToAdd)
+        {
+            // Vehicle vehicleToCharge = m_Vehicles[Convert.ToInt32(i_licensePlate)];
+            // if(vehicleToCharge.EnergySource is ElectricSource)
+            // {
+            //     ElectricSource electricSource = vehicleToCharge.EnergySource as ElectricSource;
+            //     electricSource.Charge(i_amountOfMinutesToAdd);
+            // }
+            // else
+            // {
+            //     throw new ArgumentException("Vehicle is not electric based");
+            // }
+        }
+
+
+        //todo: complete function
+
+        public StringBuilder GetVehicleDetails(string i_licensePlate)
+        {
+            StringBuilder vehicleDetails = new StringBuilder();
+            vehicleDetails.AppendLine(m_Vehicles[Convert.ToInt32(i_licensePlate)].ToString());
+            vehicleDetails.AppendLine(m_CustomersTickets[Convert.ToInt32(i_licensePlate)].ToString());
+            return vehicleDetails;
+        }
     }
 }

61429fe006d8482e9a2159e97f9ab240dbf2db70
Avi Maya <71597846+avim97@users.noreply.github.com>
2023-05-25 18:42:09 +0300

    logic

--- Ex03.GarageLogic/Models/ElectricBattery.cs
+++ Ex03.GarageLogic/Models/ElectricBattery.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ex03.GarageLogic.Models
+﻿namespace Ex03.GarageLogic.Models
 {
     internal class ElectricBattery: EnergySource
     {
--- Ex03.GarageLogic/VehicleFactory.cs
+++ Ex03.GarageLogic/VehicleFactory.cs
@@ -10,48 +10,75 @@ namespace Ex03.GarageLogic
         public static Vehicle GetVehicle(string i_VehicleType)
         {
             Vehicle vehicle = null;
-
             Enum.TryParse(i_VehicleType, ignoreCase: true, out eVehicleType vehicleType);
 
-            if (vehicleType.Equals(eVehicleType.Car))
+            EnergySource vehicleEnergySource = getEnergySource(vehicleType);
+
+            if (vehicleType.Equals(eVehicleType.ElectricCar) | vehicleType.Equals(eVehicleType.FuelCar))
             {
                 const int k_NumWheels = 5;
                 const float k_MaxWheelsPressure = 33f;
 
-                vehicle = new Car(k_NumWheels, k_MaxWheelsPressure);
+                vehicle = new Car(k_NumWheels, k_MaxWheelsPressure)
+                {
+                    EnergySource = vehicleEnergySource
+                };
             }
-            else if (vehicleType.Equals(eVehicleType.Motorcycle))
+            else if (vehicleType.Equals(eVehicleType.ElectricMotorcycle) | vehicleType.Equals(eVehicleType.FuelMotorcycle))
             {
                 const int k_NumWheels = 2;
                 const float k_MaxWheelsPressure = 31f;
 
-                vehicle = new Motorcycle(k_NumWheels, k_MaxWheelsPressure);
+                vehicle = new Motorcycle(k_NumWheels, k_MaxWheelsPressure)
+                {
+                    EnergySource = vehicleEnergySource
+                };
             }
             else if (vehicleType.Equals(eVehicleType.Truck))
             {
                 const int k_NumWheels = 14;
                 const float k_MaxWheelsPressure = 26f;
 
-                vehicle = new Truck(k_NumWheels, k_MaxWheelsPressure);
+                vehicle = new Truck(k_NumWheels, k_MaxWheelsPressure)
+                {
+                    EnergySource = vehicleEnergySource
+                };
             }
 
             return vehicle;
         }
 
-        private static EnergySource getEnergySource(string i_EnergySourceType)
+        private static EnergySource getEnergySource(eVehicleType i_VehicleType)
         {
-
             EnergySource energySourceToSet = null;
 
-            Enum.TryParse(i_EnergySourceType, ignoreCase: true, out eEnergySourceType energySourceType);
-
-            if (energySourceType.Equals(eEnergySourceType.Electric))
+            if (i_VehicleType.Equals(eVehicleType.ElectricCar))
             {
-                energySourceToSet = new ElectricBattery();
+                const float k_BatteryCapacityTime = 2.6f;
+                energySourceToSet = new ElectricBattery(k_BatteryCapacityTime);
             }
-            else if (energySourceType.Equals(eEnergySourceType.Fuel))
+            else if (i_VehicleType.Equals(eVehicleType.FuelCar))
             {
-                energySourceToSet = new FuelTank();
+                const eFuelType k_FuelType = eFuelType.Octan95;
+                const float k_TankCapacity = 46f;
+                energySourceToSet = new FuelTank(k_FuelType, k_TankCapacity);
+            }
+            else if (i_VehicleType.Equals(eVehicleType.ElectricMotorcycle))
+            {
+                const float k_BatteryCapacityTime = 5.2f;
+                energySourceToSet = new ElectricBattery(k_BatteryCapacityTime);
+            }
+            else if (i_VehicleType.Equals(eVehicleType.FuelMotorcycle))
+            {
+                const eFuelType k_FuelType = eFuelType.Octan98;
+                const float k_TankCapacity = 6.4f;
+                energySourceToSet = new FuelTank(k_FuelType, k_TankCapacity);
+            }
+            else if (i_VehicleType.Equals(eVehicleType.Truck))
+            {
+                const eFuelType k_FuelType = eFuelType.Soler;
+                const float k_TankCapacity = 135f;
+                energySourceToSet = new FuelTank(k_FuelType, k_TankCapacity);
             }
 
             return energySourceToSet;
